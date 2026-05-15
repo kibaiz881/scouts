ac7db6cbad65c68a4377c11547d6cf10c74e5345
kibaiz881 <solofo881@gmail.com>
2026-05-15 16:11:05 +0300

    vpa

--- src/Repository/VatompanorenanaRepository.php
+++ src/Repository/VatompanorenanaRepository.php
@@ -16,28 +16,28 @@ class VatompanorenanaRepository extends ServiceEntityRepository
         parent::__construct($registry, Vatompanorenana::class);
     }
 
-//    /**
-//     * @return Vatompanorenana[] Returns an array of Vatompanorenana objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('v')
-//            ->andWhere('v.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('v.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    //    /**
+    //     * @return Vatompanorenana[] Returns an array of Vatompanorenana objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('v')
+    //            ->andWhere('v.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('v.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
 
-//    public function findOneBySomeField($value): ?Vatompanorenana
-//    {
-//        return $this->createQueryBuilder('v')
-//            ->andWhere('v.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    public function findOneBySomeField($value): ?Vatompanorenana
+    //    {
+    //        return $this->createQueryBuilder('v')
+    //            ->andWhere('v.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
